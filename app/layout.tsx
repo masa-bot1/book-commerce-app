2a7c8e8eb9bc00cd01a1b53b2e077d5175c9d53f
masaki.nakano <bdyht515@yahoo.co.jp>
2025-04-20 08:52:02 +0900

    Headerコンポーネントを作成

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
@@ -19,9 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body
-        className={notoSansJP.className}
-      >
+      <body className={notoSansJP.className}>
+        <Header />
         {children}
       </body>
     </html>
